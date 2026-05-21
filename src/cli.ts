f1827972b2e04fcb9a2a3830a26ee46d56e183d0
Andrei1001 <andreidizschmitt@gmail.com>
2026-05-21 09:55:57 -0300

    criado comando add do cli

--- src/cli.ts
+++ src/cli.ts
@@ -1,1 +1,28 @@
+import TodoListClass, { Item } from "./core";
+
+
+const todoList = new TodoListClass("todolist.Json");
 const params = process.argv;
+const command = params[2];
+
+if (command === "add") {
+    const value = params[3];
+    if(!value){
+        console.error("Valor do item é obrigado");
+        process.exit(1);
+    }
+    try{
+           await todoList.addItem(new Item(value));
+    }
+    catch(error){
+        console.error("Erro ao adicionar Item:", error);
+        process.exit(10);
+    }
+    console.log(`Deu certo "${value}" `);
+    process.exit(0);
+}
+if(command)
+    console.log(`Comando não reconhecido`);
+
+
+console.log("não entrou em if nenhum");
